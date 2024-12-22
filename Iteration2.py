0dc5756ffdf1f955dd8a880ed2c08933f5ab1728
MeghanaCH09 <meghanacheruvu@gmial.com>
2024-12-22 13:07:32 +0200

    Iteration2

---
+++ Iteration2.py
@@ -0,0 +1,11 @@
+def number(n):
+    iteration=0
+    for i in range(1, n+1):
+        iteration+=1
+        print("When the value is 'N' iteration will be:", iteration)
+
+number(14)
+number(30)
+number(55)
+
+print("For each velua the time take to execute the cdoe will gradually increase")
