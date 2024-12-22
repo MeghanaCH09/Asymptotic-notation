f0715e88f025036fad23df6dd2b175ff9ac12d01
MeghanaCH09 <meghanacheruvu@gmial.com>
2024-12-22 12:55:13 +0200

    Iteration

---
+++ iteration.py
@@ -0,0 +1,9 @@
+def printnum(n):
+    iteration=0
+    print("The number is: ", n)
+    iteration+=1
+    print("Final Iteration result is: ", iteration)
+
+printnum(30)
+printnum(55)
+print("The time taken for the execution will not chnage even if the code is changes")
